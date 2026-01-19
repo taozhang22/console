137a47907977a9ff9a1601b7b4c21228fdc17c8a
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-20 01:56:55 +0800

    Create 更新conda.sh

---
+++ conda/更新conda.sh
@@ -0,0 +1,6 @@
+conda activate base
+conda update -n base -c defaults conda
+
+conda activate base
+conda update -n base -c defaults conda
+conda update -n base -c defaults ca-certificates certifi openssl
